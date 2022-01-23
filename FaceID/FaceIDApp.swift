a31c29e2c3a4bf1f55e9a745eddc597eb37ea244
ming.gu <ming.gu@gearradio.net>
2022-01-23 17:35:39 +0800

    Initial Commit

---
+++ FaceID/FaceIDApp.swift
@@ -0,0 +1,17 @@
+//
+//  FaceIDApp.swift
+//  FaceID
+//
+//  Created by  明智 on 2022/1/23.
+//
+
+import SwiftUI
+
+@main
+struct FaceIDApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
